220724e23009932d64a6936ac891c318728e232b
vaniiiii <ivandzambas@gmail.com>
2024-12-06 17:29:17 +0100

    test: Update tests with helper functions

--- test/withdraw.t.ts
+++ test/withdraw.t.ts
@@ -1,165 +1,138 @@
 import { zkit } from "hardhat";
 import { expect } from "chai";
-import { Withdraw } from "@zkit";
-import { buildPedersenHash, buildMimcSponge } from "circomlibjs";
-import MerkleTree, { Element, HashFunction } from "fixed-merkle-tree";
-import {
-  WithdrawGroth16Verifier,
-  WithdrawGroth16Verifier__factory,
-} from "../typechain-types";
+import { CalldataWithdrawGroth16, Withdraw } from "@zkit";
+import { WithdrawGroth16Verifier__factory } from "../typechain-types";
+import { WithdrawGroth16Verifier } from "../typechain-types";
 import hre from "hardhat";
+import {
+  generateCommitment,
+  pedersenHash,
+  bigIntToBuffer,
+} from "../utils/utils";
+import { createTree } from "../utils/tree";
+import { PrivateWithdrawGroth16 } from "@zkit";
 
-const leBufferToBigint = (buff: Buffer): bigint => {
-  let res = 0n;
-  for (let i = 0; i < buff.length; i++) {
-    res = res + (BigInt(buff[i]) << BigInt(i * 8));
-  }
-  return res;
-};
-
-const leBigintToBuffer = (num: bigint, byteLength: number): Buffer => {
-  if (num < 0n) throw new Error("BigInt must be non-negative");
-  const buffer = Buffer.alloc(byteLength);
-  for (let i = 0; i < byteLength; i++) {
-    buffer[i] = Number(num & 0xffn);
-    num >>= 8n;
-  }
-  return buffer;
-};
-
-describe("Withdrawal Test", () => {
-  let mimc: any;
-  let pedersen: any;
+describe("Withdraw Test", () => {
   let circuit: Withdraw;
-  let tree: MerkleTree;
-  let validInputs: any;
   let verifier: WithdrawGroth16Verifier;
 
-  before(async () => {
-    mimc = await buildMimcSponge();
-    pedersen = await buildPedersenHash();
+  beforeEach(async () => {
     circuit = await zkit.getCircuit("Withdraw");
-
     const VerifierFactory: WithdrawGroth16Verifier__factory =
       await hre.ethers.getContractFactory("WithdrawGroth16Verifier");
     verifier = await VerifierFactory.deploy();
     await verifier.waitForDeployment();
-
-    const hasher: HashFunction<Element> = (
-      left: Element,
-      right: Element
-    ): string => {
-      return mimc.F.toString(mimc.multiHash([left, right]));
-    };
-
-    tree = new MerkleTree(20, [], { hashFunction: hasher });
-    validInputs = createWithdrawInputs(BigInt(987654321), BigInt(123456789));
   });
 
-  const createWithdrawInputs = (nullifier: bigint, secret: bigint) => {
-    if (nullifier <= 0n || secret <= 0n) {
-      throw new Error("Nullifier and secret must be positive");
-    }
-
-    const pedersenHash = (data: Buffer): bigint => {
-      const pedersenOutput = pedersen.hash(data);
-      return leBufferToBigint(
-        pedersen.babyJub.F.fromMontgomery(
-          pedersen.babyJub.unpackPoint(pedersenOutput)[0]
-        )
-      );
-    };
-
-    const commitment = pedersenHash(
-      Buffer.concat([
-        leBigintToBuffer(nullifier, 31),
-        leBigintToBuffer(secret, 31),
-      ])
-    );
-
+  it("should verify witness generation and proof", async () => {
+    const { nullifier, secret, commitment } = await generateCommitment();
+    const tree = await createTree();
     tree.insert(commitment.toString());
+
     const { pathElements, pathIndices } = tree.path(
       tree.indexOf(commitment.toString())
     );
-    const nullifierHashValue = pedersenHash(leBigintToBuffer(nullifier, 31));
 
-    return {
-      root: BigInt(tree.root),
-      nullifierHash: BigInt(nullifierHashValue),
-      nullifier,
+    const input: PrivateWithdrawGroth16 = {
       secret,
+      nullifier,
+      root: BigInt(tree.root),
+      nullifierHash: await pedersenHash(bigIntToBuffer(nullifier, 31)),
       pathElements: pathElements.map((el) => BigInt(el)),
       pathIndices,
     };
-  };
 
-  it("should verify valid withdrawal with merkle proof and check constraints", async () => {
-    await expect(circuit).witnessInputs(validInputs).to.have.witnessOutputs({});
+    await expect(circuit).with.witnessInputs(input).to.have.witnessOutputs({});
+    await expect(circuit).to.generateProof(input);
 
-    const proof = await circuit.generateProof(validInputs);
-    await expect(circuit).to.verifyProof(proof);
-    await expect(circuit).to.have.constraints.within(30000, 40000);
-  });
-
-  it("should verify proof using Solidity verifier", async () => {
-    const proof = await circuit.generateProof(validInputs);
-
+    const proof = await circuit.generateProof(input);
     await expect(circuit)
       .to.useSolidityVerifier(verifier)
       .and.verifyProof(proof);
   });
 
-  it("should fail Solidity verification with corrupted proof", async () => {
-    const proof = await circuit.generateProof(validInputs);
-    const calldata = await circuit.generateCalldata(proof);
+  it("should fail with incorrect nullifier", async () => {
+    const { nullifier, secret, commitment } = await generateCommitment();
+    const tree = await createTree();
+    tree.insert(commitment.toString());
 
-    // Corrupt proof points
-    const corruptedCalldata = [...calldata];
-    corruptedCalldata[0] = [
-      "0x0000000000000000000000000000000000000000000000000000000000000001",
-      "0x0000000000000000000000000000000000000000000000000000000000000001",
-    ];
-
-    const result = await verifier.verifyProof(
-      // @ts-ignore
-      corruptedCalldata[0],
-      corruptedCalldata[1],
-      corruptedCalldata[2],
-      corruptedCalldata[3]
+    const { pathElements, pathIndices } = tree.path(
+      tree.indexOf(commitment.toString())
     );
 
-    expect(result).to.be.false;
-  });
+    const input: PrivateWithdrawGroth16 = {
+      secret,
+      nullifier: BigInt(0),
+      root: BigInt(tree.root),
+      nullifierHash: await pedersenHash(bigIntToBuffer(nullifier, 31)),
+      pathElements: pathElements.map((el) => BigInt(el)),
+      pathIndices,
+    };
 
-  it("should fail with incorrect nullifier hash", async () => {
-    const invalidInputs = { ...validInputs, nullifierHash: BigInt(123) };
-
-    await expect(
-      expect(circuit).witnessInputs(invalidInputs)
-    ).to.be.rejectedWith(
+    await expect(expect(circuit).witnessInputs(input)).to.be.rejectedWith(
       "Error in template Withdraw",
       "Wrong nullifier hash validation"
     );
   });
 
-  it("should fail with non-existent merkle path", async () => {
-    const invalidInputs = {
-      ...validInputs,
-      pathElements: validInputs.pathElements.map(() => BigInt(999)),
-      root: BigInt(888),
+  it("should fail with incorrect commitment", async () => {
+    const { nullifier, secret } = await generateCommitment();
+    const tree = await createTree();
+
+    const invalidCommitment = BigInt(1000);
+    tree.insert(invalidCommitment.toString());
+
+    const { pathElements, pathIndices } = tree.path(
+      tree.indexOf(invalidCommitment.toString())
+    );
+
+    const input: PrivateWithdrawGroth16 = {
+      secret,
+      nullifier,
+      root: BigInt(tree.root),
+      nullifierHash: await pedersenHash(bigIntToBuffer(nullifier, 31)),
+      pathElements: pathElements.map((el) => BigInt(el)),
+      pathIndices,
     };
 
-    await expect(
-      expect(circuit).witnessInputs(invalidInputs)
-    ).to.be.rejectedWith(
-      "Error in template MerkleTreeChecker",
-      "Invalid Merkle path"
+    await expect(expect(circuit).with.witnessInputs(input)).to.be.rejectedWith(
+      "Error in template MerkleTreeChecker"
     );
   });
 
-  it("should fail with invalid nullifier/secret values", async () => {
-    await expect(() => createWithdrawInputs(0n, 123n)).to.throw(
-      "Nullifier and secret must be positive"
+  it("should fail with tampered proof", async () => {
+    const { nullifier, secret, commitment } = await generateCommitment();
+    const tree = await createTree();
+    tree.insert(commitment.toString());
+
+    const { pathElements, pathIndices } = tree.path(
+      tree.indexOf(commitment.toString())
     );
+
+    const input: PrivateWithdrawGroth16 = {
+      secret,
+      nullifier,
+      root: BigInt(tree.root),
+      nullifierHash: await pedersenHash(bigIntToBuffer(nullifier, 31)),
+      pathElements: pathElements.map((el) => BigInt(el)),
+      pathIndices,
+    };
+
+    const proof = await circuit.generateProof(input);
+    const calldata: CalldataWithdrawGroth16 =
+      await circuit.generateCalldata(proof);
+
+    const tamperedCalldata: CalldataWithdrawGroth16 = [
+      [calldata[0][1], calldata[0][0]],
+      calldata[1],
+      calldata[2],
+      calldata[3],
+    ];
+
+    const validResult = await verifier.verifyProof(...calldata);
+    expect(validResult).to.be.true;
+
+    const invalidResult = await verifier.verifyProof(...tamperedCalldata);
+    expect(invalidResult).to.be.false;
   });
 });
